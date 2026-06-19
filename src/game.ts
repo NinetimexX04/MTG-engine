7accbd0edd8b679db01bde4de6441c8926e52c24
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-06-18 19:19:37 -0600

    feat: define game phases and turn steps enums

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,26 @@
+export enum Phase {
+  Beginning = 'BEGINNING',
+  Main = 'MAIN',
+  Combat = 'COMBAT',
+  Ending = 'ENDING',
+}
+
+export enum Step {
+  Untap = 'UNTAP',
+  Upkeep = 'UPKEEP',
+  Draw = 'DRAW',
+  PrecombatMain = 'PRECOMBAT_MAIN',
+  BeginningOfCombat = 'BEGINNING_OF_COMBAT',
+  DeclareAttackers = 'DECLARE_ATTACKERS',
+  DeclareBlockers = 'DECLARE_BLOCKERS',
+  CombatDamage = 'COMBAT_DAMAGE',
+  EndOfCombat = 'END_OF_COMBAT',
+  PostcombatMain = 'POSTCOMBAT_MAIN',
+  EndStep = 'END_STEP',
+  Cleanup = 'CLEANUP',
+}
 
 export class Game {
+  static readonly MAX_PLAYERS = 2;
 
 }
