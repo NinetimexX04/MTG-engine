6f3e6fb8d4137b06e6e1b12c3fbabdf7c40a3911
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-18 19:04:06 -0600

    feat(app): render sample Card in main area

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 import Header from './components/Header'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className='app-container'>
         <Header />
         <main>
-
+          <Card url='https://cards.scryfall.io/normal/front/f/3/f353281a-0e56-448f-b41a-beb3949c5f11.jpg?1783928301' name='Twinblade Assassins'/>
         </main>
         <footer>
 
