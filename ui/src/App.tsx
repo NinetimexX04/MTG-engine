ad6bdde8fb997dbbf7bc1e807ebdd914f63489d0
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-18 19:04:01 -0600

    refactor(app): render Header component instead of inline markup

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='app-container'>
-        <header>
-          <nav className='navigation'>
-            <ul>
-              <li></li>
-            </ul>
-          </nav>
-        </header>
+        <Header />
         <main>
 
         </main>
