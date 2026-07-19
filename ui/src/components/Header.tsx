7fa2fe109540aa85e0cfd25d5498df182f3f31c0
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-18 19:03:26 -0600

    feat(header): add Header component with logo and nav placeholders

---
+++ ui/src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <div id='logo'>
+                
+            </div>
+            <nav>
+                <ul>
+                    <li><a src='#Home'></a></li>
+
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
