ced035d79060fe2e1e370e530b76f10c8b49f970
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-18 19:03:25 -0600

    feat(permanent): add Permanent class wrapping Card with battlefield state

---
+++ src/permanent.ts
@@ -0,0 +1,37 @@
+import { Card } from "./card.js"
+import { Counter } from "./counters.js";
+
+export class Permanent {
+    card: Card;
+    isTapped: boolean;
+    counters: Counter[];
+
+    constructor(card: Card, isTapped: boolean = false, counters: Counter[] = []) {
+        if (!(card instanceof Card)) {
+            throw new Error('card must be a valid Card object.')
+        }
+        if (!Array.isArray(counters)) {
+            throw new Error('Counters must be an array.');
+        }
+        if (!(typeof isTapped === 'boolean')) {
+            throw new Error('isTapped needs to be a boolean vlaue.')
+        }
+        this.card = card;
+        this.isTapped = isTapped;
+        this.counters = counters;
+    }
+
+    addCounter() {
+        this.counters.push(new Counter());
+    }
+
+    tap() {
+        //logic
+    }
+
+    untap() {
+        //logic
+    }
+
+
+}
