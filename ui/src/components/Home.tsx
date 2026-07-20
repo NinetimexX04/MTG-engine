d010d8f54261ef2b57ee72a2bd33b72456b5d512
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-19 21:35:15 -0600

    feat(home): add Home page component

---
+++ ui/src/components/Home.tsx
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Welcome to MTG sandbox</h1>
+            <h2>- by Alexander Van der Merwe</h2>
+            <button>Play now</button>
+        </div> 
+    );
+}
+
+export default Home;
