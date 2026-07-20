ccfbc22870ed8ac6e71db154e563048501575b9d
Alexander Van der Merwe <ninetime@Alexanders-MacBook-Pro.local>
2026-07-19 21:36:06 -0600

    feat(header): display MTG logo in header

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,8 +1,10 @@
+import logo from '../assets/logo.svg'
+
 function Header() {
     return (
         <header>
             <div id='logo'>
-                
+                <img src={logo}/>
             </div>
             <nav>
                 <ul>
