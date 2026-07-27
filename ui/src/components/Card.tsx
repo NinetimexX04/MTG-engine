3d5d18e16d978e3f20ef8536d8b624fd0e2ee2fa
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2026-07-27 16:14:34 -0600

    fix(card): apply leftMargin to card image element

--- ui/src/components/Card.tsx
+++ ui/src/components/Card.tsx
@@ -1,7 +1,7 @@
 function Card({ url, name, leftMargin = 0 }: { url: string, name: string, leftMargin?: number }) {
     return (
         <div className="card" style={{ marginLeft: leftMargin }}>
-            <img src={url} alt={name} />
+            <img src={url} alt={name} style={{ marginLeft: leftMargin }}/>
         </div>
     );
 }
