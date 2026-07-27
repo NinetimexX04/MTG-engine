99e902dee3c54463d2aa92d57ffee24e09b8afd3
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2026-07-27 16:14:36 -0600

    feat(choosedeck): return parsed decklist from handleFileChange

--- ui/src/components/ChooseDeck.tsx
+++ ui/src/components/ChooseDeck.tsx
@@ -5,9 +5,7 @@ function ChooseDeck() {
         const file = event.target.files?.[0];
         if (!file) return;
         const text = await file.text();
-        console.log(text);
-        const cards = parseDecklist(text);
-        console.log(cards);
+        return parseDecklist(text);
     }
 
     return (
