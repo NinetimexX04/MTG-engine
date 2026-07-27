40922ee898c0b9de97cdb4eff4cd49447583d085
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2026-07-27 16:14:26 -0600

    feat(create-board): add server-side board creation from decklist
    Fetches full Scryfall data for a parsed decklist, splits off the last card
    as commander (matching Moxfield's export convention), shuffles the rest
    into a Library, and draws an opening Hand.

---
+++ src/createBoard.ts
@@ -0,0 +1,41 @@
+import type { CardData } from '../ui/src/components/type.js'
+import { getCardByName } from "./services/scryfallClient.js";
+import { Card } from "./card.js";
+import { Commander } from "./commander.js";
+import { Library } from "./library.js";
+import { Graveyard } from "./graveyard.js";
+import { Hand } from "./hand.js";
+import { CardPile } from "./cardpile.js";
+
+type Board = {
+    commanderCard: Commander;
+    libraryCards: Library;
+    handCards: Hand;
+}
+
+async function createBoard(cards: CardData[]): Promise<Board> {
+    if (!cards || !Array.isArray(cards)) {
+        throw new Error('Cards must be an array with length of 100.')
+    }
+
+    const libraryCardsArray = [];
+    for (let i=0; i < cards.length; i++) {
+        let ScryfallCard = await getCardByName(cards[i].name);
+        let card = Card.fromScryfallData(ScryfallCard);
+        libraryCardsArray.push(card);
+    }
+    const library = (new Library(libraryCardsArray));
+    const commanderCard = new Commander(library.removeAt(library.cards.length-1, 1)[0])
+    library.shuffle();
+    const hand = new Hand(library.removeTop(7));
+
+    const board: Board = {
+        commanderCard: commanderCard,
+        libraryCards: library,
+        handCards: hand
+    }
+
+    return board;
+}
+
+export default createBoard;
