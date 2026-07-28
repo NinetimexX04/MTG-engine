791b2d7a307ee9faf7b1084d43ebb2c5cacd0375
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2026-07-28 16:00:08 -0600

    feat(hand): pass card instanceId to Card component
    Also updates the CardData import to the new top-level types/ location.

--- ui/src/components/Hand.tsx
+++ ui/src/components/Hand.tsx
@@ -1,5 +1,5 @@
 import Card from "./Card";
-import type { CardData } from "./type";
+import type { CardData } from "../../../types/type";
 
 function Hand({ cards }: { cards: CardData[] }) {
     const maxOverlap = 100;
@@ -7,7 +7,7 @@ function Hand({ cards }: { cards: CardData[] }) {
     return (
         <div id='hand' className="zone">
             {cards.map((card, index) => (
-                <Card key={index} url={card.url} name={card.name} leftMargin={ index === 0 ? 0 : -overlap }/>
+                <Card key={index} url={card.url} name={card.name} leftMargin={ index === 0 ? 0 : -overlap } id={card.instanceId}/>
             ))}
         </div>
     );
