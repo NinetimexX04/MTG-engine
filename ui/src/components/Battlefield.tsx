e7aa12aa49c7c3eb8bd260080f2453b9d5643ded
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2026-07-28 16:00:02 -0600

    feat(battlefield): pass card instanceId to Card component
    Also updates the CardData import to the new top-level types/ location.

--- ui/src/components/Battlefield.tsx
+++ ui/src/components/Battlefield.tsx
@@ -1,11 +1,11 @@
 import Card from "./Card";
-import type { CardData } from "./type";
+import type { CardData } from "../../../types/type";
 
 function Battlefield({ cards }: { cards: CardData[] }) {
     return (
         <div id='battlefield' className="zone">
             {cards.map((card, index) => (
-                <Card key={index} url={card.url} name={card.name} />
+                <Card key={index} url={card.url} name={card.name} id={card.instanceId}/>
             ))}
         </div>
     );
